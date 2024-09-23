6920737f98427cb6dab545f575b1645b0c405c3f
abc <abc@gmail.com>
2024-09-23 09:23:23 +0530

    bhumi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from modules.interpreter import interpret
 
 program_filepath = sys.argv[1]
 
-# read file lines 
+
 program_lines = []
 with open(program_filepath, "r") as program_file:
     program_lines = [line.strip() for line in program_file.readlines()]
